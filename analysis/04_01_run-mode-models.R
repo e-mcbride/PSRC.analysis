a47499d9d48334aa239dc6af88838c16b26ff47b
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-08-31 13:14:41 -0700

    Added instructions and fixed a type

--- analysis/04_01_run-mode-models.R
+++ analysis/04_01_run-mode-models.R
@@ -3,8 +3,12 @@
 library(here)
 library(MplusAutomation)
 
+# NOTE: before running the following, all .inp files in "analysis/03_Mplus/mode/template"
+#       must be manually copied to "analysis/03_Mplus/mode/". This is to protect any changes
+#       made to the .inp files from being overwritten by 02_01 script when the template
+#       file is used to build the initial .inp files.
 MplusAutomation::runModels(
-  here("analysis/03_Mplus/mode/",
-       recursive=TRUE))
+  here("analysis/03_Mplus/mode/"),
+       recursive=TRUE)
 
 
