abffbd7a6c143d93897cf4e42373d35a35198d1c
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-10-05 10:35:51 -0700

    Changes to optimal matching code

--- analysis/04_seq-analysis_travbeh/04_05_optimal-matching.R
+++ analysis/04_seq-analysis_travbeh/04_05_optimal-matching.R
@@ -1,12 +1,12 @@
 # optimal matching (OM)
 
 library(tidyverse); library(here);library(TraMineR)
-read_rds(here("analysis/data/derived_data/pl_seq.rds"))
+pl.seq <- read_rds(here("analysis/data/derived_data/pl_seq.rds"))
 
-costs <- TraMineR::seqsubm(seq_samp05, method = "TRATE")#, with.missing = TRUE) # took about 60 seconds
-round(couts, 2)
+costs <- TraMineR::seqsubm(pl.seq, method = "TRATE", with.missing = TRUE) # OLD NOTE: took about 60 seconds
+round(costs, 2)
 
-pl_om <- TraMineR::seqdist(seq_samp05, method = "OM", indel = 3, sm = couts)#, with.missing = TRUE)
-# with 5000 households (12704 people), it took 1.135 days (about 27 hrs)
-
-write_rds(pl_om, here("data", "sequences-optimal-matching.rds"))
+pl_om <- TraMineR::seqdist(pl.seq, method = "OM", indel = 3, sm = costs, with.missing = TRUE)
+# OLD NOTE: with 5000 households (12704 people), it took 1.135 days (about 27 hrs)
+# NEW NOTE: started: 10/4/2021 22:10
+write_rds(pl_om, here("analysis/data/derived_data/sequences-optimal-matching.rds"))
