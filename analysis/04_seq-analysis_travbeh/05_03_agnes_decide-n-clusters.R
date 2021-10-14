29284b3824df44d2bc6fbd0416d30ce0669ce48a
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-10-14 16:58:18 -0700

    decided number of clusters, updated script to write rds

--- analysis/04_seq-analysis_travbeh/05_03_agnes_decide-n-clusters.R
+++ analysis/04_seq-analysis_travbeh/05_03_agnes_decide-n-clusters.R
@@ -39,9 +39,6 @@ cluster_id <- alldata %>% select(personid, starts_with("nclust"))
 
 # join cluster ids to the sequences by pid. make data "long" for ggplot
 
-#
-# cl_choice <- cluster_id %>%
-#   select(personid, nclust2)
 
 seq_clust_long <- pl.seq.5min %>%
   as_tibble(rownames = "personid") %>%
@@ -104,10 +101,31 @@ ggsave(filename = here::here("analysis/figures/plot_7clust.png"), plot_c7)
 ggsave(filename = here::here("analysis/figures/plot_8clust.png"), plot_c8)
 
 
+# six cluster decision ==========================================================
+level_key <- list(type1 = "Home Day",
+                  type2 = "Typical Work Day",
+                  type3 = "School Day",
+                  type4 = "Other Outings Day",
+                  type5 = "Atypical Work Day",
+                  type6 = "Traveling")
+
+sixclust <- cluster_id %>% select(personid, c6 = nclust6) %>%
+  mutate(c6str = factor(c6, labels = paste0("type", 1:6))) %>%
+  mutate(namedcluster = recode_factor(c6str, !!!level_key)) %>%
+  select(-c6str)
+
+write_rds(sixclust, here::here("analysis/data/derived_data/six-cluster-by-pids.rds"))
+
+# cluster <- cutree(clusterward, 6)%>%
+#   factor(labels=paste0("type", 1:6))
+#
+# alldata <- cbind(grp_en_tu, cluster) %>%
+#   mutate(namedcluster = recode_factor(cluster, !!!level_key))
+
 # seqplot <- seqfplot(pl.seq.5min, group = cl6)
 #
 # seqplot
 #
 # seqmtplot(pl.seq.5min, group = cl6)
 
-# alldata <- cbind(grp_en_tu, allclusters)
+
