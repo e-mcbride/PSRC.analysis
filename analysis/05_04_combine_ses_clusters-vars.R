af75013b1c80bccfd30e1b17f65ab3bbe157d608
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-10-23 13:39:58 -0700

    quick upload

--- analysis/05_04_combine_ses_clusters-vars.R
+++ analysis/05_04_combine_ses_clusters-vars.R
@@ -1,22 +1,26 @@
 # Building cluster variables and modifying SES variables, then combining
 library(tidyverse)
 
-clusterward <- read_rds(here::here("analysis/data/derived_data/cluster-ward_5min.rds"))
+# clusterward <- read_rds(here::here("analysis/data/derived_data/cluster-ward_5min.rds"))
+#
+# # final clustering decision ================================
+# cluster <- cutree(clusterward, k = 6) %>%
+#   factor(labels=paste("Type", 1:6))
+#
+# # level_key <- list(`Type 1` = "Home Day",
+# #                   `Type 2` = "School Day",
+# #                   `Type 3` = "Typical Work Day",
+# #                   `Type 4` = "Errands Type 1",
+# #                   `Type 5` = "Mostly Out of Home",
+# #                   `Type 6` = "Errands Type 2",
+# #                   `Type 7` = "Non-typical Work Day",
+# #                   `Type 8` = "Leave Home",
+# #                   `Type 9` = "Traveling")
+#
 
-# final clustering decision ================================
-cluster <- cutree(clusterward, k = 6) %>%
-  factor(labels=paste("Type", 1:6))
 
-level_key <- list(`Type 1` = "Home Day",
-                  `Type 2` = "School Day",
-                  `Type 3` = "Typical Work Day",
-                  `Type 4` = "Errands Type 1",
-                  `Type 5` = "Mostly Out of Home",
-                  `Type 6` = "Errands Type 2",
-                  `Type 7` = "Non-typical Work Day",
-                  `Type 8` = "Leave Home",
-                  `Type 9` = "Traveling")
 
+pl.seq.5min <- read_rds(here::here("analysis/data/derived_data/pl_seq_5min.rds"))
 
 # minutes by state
 minbystate <- pl.seq.5min %>%
@@ -27,3 +31,26 @@ minbystate <- pl.seq.5min %>%
   ungroup() %>%
   spread(key = place, value = "minutes", fill = 0) %>%
   rename(min_NA = `*`, min_G = "Gshop", min_H = "Home", min_O = "Other", min_S = "School", min_T = "Travel", min_W = "Work")
+
+sixclust <- read_rds(here::here("analysis/data/derived_data/six-cluster-by-pids.rds")) %>%
+  rename(clustnum = c6)
+
+frag_vars <- minbystate %>%
+  left_join(sixclust, by = "personid")
+
+grp_en_tu <- read_rds(here::here("analysis/data/derived_data/grouping-entropy-turbulence.rds"))
+
+grp_frag_trav <- grp_en_tu %>%
+  left_join(frag_vars, by = "personid")
+
+write_rds(grp_frag_trav, here::here("analysis/data/derived_data/grouping_ent-tur_traveldat.rds"))
+
+# make dataset with factors converted to numeric
+factors2numeric <- grp_frag_trav %>%
+  mutate(across(where(is.factor), ~ as.numeric(.x)))
+
+# data.frame(levels(grp_frag_trav$employment), as.num)
+#
+# x <- levels(grp_frag_trav$employment) %>%
+#   as_tibble()
+# grp
---
+++ analysis/05_05_buildgrp.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(readr)
+cluster_id <- read_rds(here::here("analysis/data/derived_data/all-clusters-by-pid.rds"))
+
+
+# six cluster decision ==========================================================
+level_key <- list(type1 = "Home Day",
+                  type2 = "Typical Work Day",
+                  type3 = "School Day",
+                  type4 = "Other Outings Day",
+                  type5 = "Atypical Work Day",
+                  type6 = "Traveling")
+
+sixclust <- cluster_id %>% select(personid, c6 = nclust6) %>%
+  mutate(c6str = factor(c6, labels = paste0("type", 1:6))) %>%
+  mutate(namedcluster = recode_factor(c6str, !!!level_key)) %>%
+  select(-c6str)
+
+write_rds(sixclust, here::here("analysis/data/derived_data/six-cluster-by-pids.rds"))
+
+# cluster <- cutree(clusterward, 6)%>%
+#   factor(labels=paste0("type", 1:6))
+#
+# alldata <- cbind(grp_en_tu, cluster) %>%
+#   mutate(namedcluster = recode_factor(cluster, !!!level_key))
+
+# seqplot <- seqfplot(pl.seq.5min, group = cl6)
+#
+# seqplot
+#
+# seqmtplot(pl.seq.5min, group = cl6)
