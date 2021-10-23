99efbcd84bcf1c38e9e351b579e64d4a23ecd9e3
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-10-22 19:52:37 -0700

    Fixed how non-travelers are added to the dataset for Mplus

--- analysis/02_01_prep-modedat_modelsyntax.R
+++ analysis/02_01_prep-modedat_modelsyntax.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 library(here)
 library(janitor)
-library(MplusAutomation)
+# library(MplusAutomation)
 
 devtools::load_all(helpers = FALSE)
 
@@ -13,17 +13,77 @@ trraw <- PSRC.data::trdat #read_rds(here("analysis/data/derived_data/trdat.rds")
 # people who made no trips are missing from the `travel` data, so I add them in with the following
 prraw <- PSRC.data::prdat #read_rds(here("analysis/data/derived_data/prdat.rds"))
 
+
+
+# Bring in ids that are NOT in OG trdat =======================================================
+
+tr_pid <- trraw %>%
+  dplyr::pull(personid) %>%
+  unique()
+
+
+# 1.  Get pids of all hh members where whole hh was missing from trraw
+### Get the hhids that are not present in trraw. These are hhs w/ nobody who traveled
+tr_hid <- trraw %>%
+  pull(hhid) %>%
+  unique()
+
+pr_hid <- prraw %>%
+  pull(hhid) %>%
+  unique()
+
+hhs_notr_hid <- pr_hid[!(pr_hid %in% tr_hid)] # hhids
+
+
+### get pids
+hhs_notr_pid <- prraw %>%
+  filter(hhid %in% hhs_notr_hid) %>%
+  pull(personid) %>%
+  unique()
+# ^ these can be safely added to the travel data
+
+
+# 2. Get the records where somebody from a hh in trdat ("clean" dataset) didn't travel.
+clean_hids <- read_rds(here("analysis/data/derived_data/clean_hids.rds"))
+
+# get pids of ppl who didn't record travel
+pr_notr_pid  <- prraw %>%
+  filter(!(personid %in% tr_pid)) %>%
+  filter(hhid %in% clean_hids) %>%
+  pull(personid) %>%
+  unique()
+
+# 3. Combine people whose entire hh is not present in trdat + ppl who do come from hhs with travelers
+
+notr_pid_combo <- c( hhs_notr_pid, pr_notr_pid) %>% unique()
+
+rm(list = setdiff(ls(), c("prraw", "trraw", "notr_pid_combo")))
+
+#  ===============================================================
+
 # import clean pids
 
-cleanpids <- read_rds(here("analysis/data/derived_data/clean_pids.rds"))
+cleanpids <- read_rds(here("analysis/data/derived_data/clean_pids.rds")) %>%
+  # add the new pid list to these
+  c(notr_pid_combo) %>%
+  unique()
 
+
+# Save person- and hh-lvl datasets (or ids) that has the new final list of ppl and hhs
+write_rds(cleanpids, here("analysis/data/derived_data/clean_pids_notr.rds"))
+
+# filter
 prdat <- prraw %>% filter(personid %in% cleanpids)
 trdat <- trraw %>% filter(personid %in% cleanpids)
 
-# Function version of above ############################################
+
 mode <- trdat %>%
-  add_nontravelers(prdat, main_mode) %>% # my function to add non-travelers
-  select(personid, sov, hov, transit, everything())
+  # my function to make dummy variables from one variable in the dataset (`main_mode`)
+  make_dummies(main_mode) %>%
+
+  # my fn to add the non-travelers to the dummy dataset
+  add_nontravelers(prraw = prraw, notr_pids = notr_pid_combo)
+
 
 # Create model syntax ##################################################################
 
