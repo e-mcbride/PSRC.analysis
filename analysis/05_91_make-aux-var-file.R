a974a0abb7c52f2fffc4318294ad845648d7ec00
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-10-25 13:01:48 -0700

    added message to a fxn

--- analysis/05_91_make-aux-var-file.R
+++ analysis/05_91_make-aux-var-file.R
@@ -3,6 +3,7 @@ filelist <- list.files(here::here("analysis/"), pattern = "05_0", full.names = T
 i <- 0
 
 for(i in 1:length(filelist)) {
+  message(paste0("Running File ", i, " of ", length(filelist)))
   source(here::here(filelist[i]))
   rm(list = dplyr::setdiff(ls(), c("filelist", "i")))
   message(paste0("File ", i, " successfully run"))
