f1579157ec3cc630af1db082b681e87d9cdc0d5e
Elizabeth McBride <mcbrideelizabethc@gmail.com>
2021-11-14 18:08:23 -0800

    Moved the function to pull results from mplus to excel to its own script

--- analysis/unneeded_unfinished/export-alternative-parameter-specs.R
+++ analysis/unneeded_unfinished/export-alternative-parameter-specs.R
@@ -7,204 +7,48 @@ c6_models <-
   MplusAutomation::readModels(here::here("analysis/Mplus/mode_cleaned_aux/final-model-work-c6/"),
              recursive = FALSE
   ) %>%
-  purrr::set_names(~ .x %>%
+  set_names(~ .x %>%
                      str_replace(".out", replacement = "") %>%
                      str_split("_cleaned_") %>%
                      map(2) %>%
                      str_replace("aux_03_", "m")
   )
 
+# read in my function `alt_param` to extract the alternative parameterizations from a `mplus.model`:
+source(here::here("analysis/90_01_fxn_extract-mplus-alternative-parameterizations.R"))
 
 # Model 3A ##########################################################
 
-m3a <- c6_models$m03a_fem.seq.interactions
 
-source(here::here("analysis/90_01_fxn_extract-mplus-alternative-parameterizations.R"))
+## female * sequences ===================================
+# c6_models$m03a_fem.seq.interactions %>%
+#   alt_param(refClass = 4, modelID = "M3A_fem_sq_rmcarless_nadlt") %>%
+#   write.table("clipboard", sep = "\t", row.names = FALSE, col.names = FALSE)
 
-aparam4 <- alt_param(mplusModel = m3a, refClass = 4, modelID = "hihitest")
+# m3a_ap4 <- m3a %>%
+#   alt_param(refClass = 4, modelID = "M3A_fem_sq_rmcarless_nadlt")
 
-# m3a <- c6_models$m03a_lic.seq.interactions
+## female * complexity
 
-# params <- m3a$parameters$unstandardized
-#
-# out <- m3a$output
-#
-# altparam.start <- which(out == "ALTERNATIVE PARAMETERIZATIONS FOR THE CATEGORICAL LATENT VARIABLE REGRESSION")
-# altparam.end <- (which(out == "ODDS RATIO FOR THE ALTERNATIVE PARAMETERIZATIONS FOR THE CATEGORICAL LATENT VARIABLE REGRESSION")-1)
-#
-# altparam <- out[altparam.start:altparam.end]
-# #
-# # altparam %>%
-# #   str_replace()
-#
-# ref.indx <- altparam %>%
-#   str_which("Parameterization using Reference Class |Intercepts")
-#
-# altparam[ref.indx]
-#
-# divide_text <- function(text.inp, ref.index) {
-#   ap_comp <- list()
-#   ap_return <- list()
-#   for (i in 1:length(ref.index)) {
-#     start <- (ref.index[i])+1
-#     if(i == length(ref.index)) {
-#       end <- length(text.inp)
-#     } else {end <- (ref.index[i+1])-1}
-#
-#     ap_comp[i] <- list(text.inp[start:end])
-#
-#     ap_return[i] <- list(ap_comp[[i]][(which(ap_comp[[i]] != ""))])
-#     names(ap_return)[i] <- text.inp[start-1]
-#   }
-#   return(ap_return)
-# }
-#
-#
-# ap <- divide_text(altparam, ref.indx)
-#
-#
-# # divide by " C#\\d      ON" ==============================================
-#
-#
-# is_ap_int <- ap %>%
-#   names() %>%
-#   str_detect("Intercept")
-#
-# ap_aux <- ap[!is_ap_int]
-# ap_int <- ap[is_ap_int]
-#
-# # ref.c <- ap_aux %>%
-# #   map(~str_which(.x, " C#\\d      ON"))
-#
-# ap_auxtabs <- ap_aux %>%
-#   map(~ divide_text(.x, str_which(.x, " C#\\d      ON")) %>% set_names(~ str_replace(., "#", ""))
-#       ) %>%
-#   modify_depth(1, ~.x %>%
-#                  map(~.x %>% str_trim() %>% strsplit("\\s+")
-#                      ) %>%
-#                  map(~ .x %>%  transpose() %>% set_names(c("VarID", "Estimate", "S.E.", "Est./S.E.", "P-Value")) %>%
-#                        as_tibble()
-#                  ) %>%
-#                  enframe(name = "ClassID") %>%
-#                  unnest(cols = c(value)) %>%
-#                  unnest(cols = c(VarID, Estimate, S.E., `Est./S.E.`, `P-Value`)) %>%
-#                  janitor::clean_names()
-#                )
-#
-#
-# ap_inttabs <- ap_int %>%
-#   map( ~ .x %>% str_trim() %>% strsplit("\\s+") %>%
-#          transpose() %>%
-#          set_names(c("VarID", "Estimate", "S.E.", "Est./S.E.", "P-Value")) %>%
-#          map(unlist) %>%
-#          as.data.frame() %>%
-#          janitor::clean_names()
-#   )
-#
-# refclass <- 4
-#
-# intparam_4 <- ap_inttabs[[refclass]] %>%
-#   mutate(class_id = "Intercept") %>%
-#   select(-var_id, everything(), var_id)
-#
-# aparam_4 <- ap_auxtabs[[refclass]] %>%
-#   select(-class_id, -var_id, everything(), class_id, var_id) %>%
-#   bind_rows(intparam_4)
-#
-# aparam_4
-#
-
-
-
-# function-ize ##################################
-
-# mplusModel <- c6_models$m03a_fem.seq.interactions
-
-divide_text <- function(text.inp, ref.index) {
-  ap_comp <- list()
-  ap_return <- list()
-  for (i in 1:length(ref.index)) {
-    start <- (ref.index[i])+1
-    if(i == length(ref.index)) {
-      end <- length(text.inp)
-    } else {end <- (ref.index[i+1])-1}
-
-    ap_comp[i] <- list(text.inp[start:end])
-
-    ap_return[i] <- list(ap_comp[[i]][(which(ap_comp[[i]] != ""))])
-    names(ap_return)[i] <- text.inp[start-1]
-  }
-  return(ap_return)
+to_table_process <- function(mplusModel, model.ID) {
+  mplusModel %>%
+    alt_param(refClass = 4, modelID = model.ID) %>%
+    # mutate(var_id = if_else(var_id == "NUFVHS", "NUFVHSLIC", var_id)) %>%
+    write.table("clipboard", sep = "\t", row.names = FALSE, col.names = FALSE)
 }
 
+c6_models$m03a_fem.cmplx.interactions %>%
+  to_table_process(model.ID = "M3A_fem_cmplx_nuflic2")
 
-# Model fxn (make it easier to run)
-#' @param mplusModel A single mplus.model object (from `MplusAutomation`)
-#' @param refClass Integer of the reference class for which you want the alternative parameterization results
-#' @param modelID A string with the unique ID code you want for this model when you paste it into excel sheet
-#'
-alt_param <- function(mplusModel, refClass, modelID){
-  out <- mplusModel$output
-
-  altparam.start <- which(out == "ALTERNATIVE PARAMETERIZATIONS FOR THE CATEGORICAL LATENT VARIABLE REGRESSION")
-  altparam.end <- (which(out == "ODDS RATIO FOR THE ALTERNATIVE PARAMETERIZATIONS FOR THE CATEGORICAL LATENT VARIABLE REGRESSION")-1)
-
-  altparam <- out[altparam.start:altparam.end]
-
-  ref.indx <- altparam %>%
-    str_which("Parameterization using Reference Class |Intercepts")
+# Model 0 ##########################################################
+c6_models$m00_baseline %>%
+  to_table_process(model.ID = "M0_baseline")
 
 
-  ap <- divide_text(altparam, ref.indx)
+# Model 1 ##########################################################
+c6_models$m01_ses %>%
+  totable_process(model.ID = "M1_SES")
 
-  is_ap_int <- ap %>%
-    names() %>%
-    str_detect("Intercept")
-
-  ap_aux <- ap[!is_ap_int]
-  ap_int <- ap[is_ap_int]
-
-  ap_auxtabs <- ap_aux %>%
-    map(~ divide_text(.x, str_which(.x, " C#\\d      ON")) %>% set_names(~ str_replace(., "#", ""))
-    ) %>%
-    modify_depth(1, ~.x %>%
-                   map(~.x %>% str_trim() %>% strsplit("\\s+")
-                   ) %>%
-                   map(~ .x %>%  transpose() %>% set_names(c("VarID", "Estimate", "S.E.", "Est./S.E.", "P-Value")) %>%
-                         as_tibble()
-                   ) %>%
-                   enframe(name = "ClassID") %>%
-                   unnest(cols = c(value)) %>%
-                   unnest(cols = c(VarID, Estimate, S.E., `Est./S.E.`, `P-Value`)) %>%
-                   janitor::clean_names()
-    )
-
-  ap_inttabs <- ap_int %>%
-    map( ~ .x %>% str_trim() %>% strsplit("\\s+") %>%
-           transpose() %>%
-           set_names(c("VarID", "Estimate", "S.E.", "Est./S.E.", "P-Value")) %>%
-           map(unlist) %>%
-           as.data.frame() %>%
-           janitor::clean_names()
-    )
-
-  intparam_ref <- ap_inttabs[[refClass]] %>%
-    mutate(class_id = "Intercept") %>%
-    select(-var_id, everything(), var_id)
-
-  aparam_ref <- ap_auxtabs[[refClass]] %>%
-    select(-class_id, -var_id, everything(), class_id, var_id) %>%
-    bind_rows(intparam_ref) %>%
-    mutate(model.ID = modelID,
-           class.name = "",
-           var.name = "") %>%
-    select(model.ID, class.name, var.name, everything())
-
-  return(aparam_ref)
-
-}
-
-aparam_4 <- alt_param(mplusModel = m3a, refClass = 4, modelID = "himodeltest")
-
-aparam_4 %>%
-  write.table("clipboard", sep = "\t", row.names = FALSE, col.names = FALSE)
+# Model 2 ##########################################################
+c6_models$m02_sesatts %>%
+  to_table_process("M2_SES_atts")
