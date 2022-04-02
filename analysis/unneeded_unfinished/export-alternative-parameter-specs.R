717d10b8b2462998907e01fbb554068d81817c9a
e-mcbride <mcbrideelizabethc@gmail.com>
2022-04-02 15:55:40 -0700

    Added more to the alternative parameter specs export file

--- analysis/unneeded_unfinished/export-alternative-parameter-specs.R
+++ analysis/unneeded_unfinished/export-alternative-parameter-specs.R
@@ -34,14 +34,23 @@ c6_models$m03a_fem.seq.interactions %>%
 # m3a_ap4 <- m3a %>%
 #   alt_param(refClass = 4, modelID = "M3A_fem_sq_rmcarless_nadlt")
 
-## female * complexity ==========================================
+## Model 3 female * complexity ==========================================
 
 c6_models$m03a_fem.cmplx.interactions %>%
-  to_table_process(model.ID = "M3A_fem_cmplx")
+  to_table_process(model.ID = "M3A_fem_cmplx_seqfix")
 
+# c6_models$m03a_fem.cmplx.interactions %>%
+#   alt_param(refClas = 2, modelID = "M3_fem_cmplx_refcl2") %>%
+#   write.table("clipboard", sep = "\t", row.names = FALSE, col.names = FALSE)
+
+c6_models$m03a_mal.cmplx.interactions %>%
+  to_table_process(model.ID = "M3_mal_cmplx")
+
+c6_models$m03a_fem.mult.interactions %>%
+  to_table_process(model.ID = "M3_fem_multiple")
 # Model 0 ##########################################################
 c6_models$m00_baseline %>%
-  to_table_process(model.ID = "M0_baseline2")
+  to_table_process(model.ID = "M0_baseline_sqfix")
 
 ## trying different reference class to see what happens
 c6_models$m00_baseline %>%
@@ -67,19 +76,41 @@ View(m01_hi_cor)
 c6_models$m02_sesatts %>%
   to_table_process("M2_SES_atts")
 
+
+# Correlation Matrix ########################################
 m02_cor <- c6_models$m02_sesatts$sampstat$correlations
+View(m02_cor)
+
+m02_cor %>%  write.table("clipboard", sep = "\t", na = "")
 
 m02_hi_cor <- ((abs(m02_cor) > 0.09) * m02_cor)
 View(m02_hi_cor)
 
-m02_hi_cor %>% write.table("clipboard", sep = "\t")
+m02_hi_cor %>% write.table("clipboard", sep = "\t", na = "")
+
+# try again
+cormat <- c6_models$aux_90_correlation.matrix$sampstat$correlations
+
+bbb <- ((cormat != 1) * cormat)
+View(bbb)
+
+View(cormat)
+
+
+bbb %>% write.table("clipboard", sep = "\t", na = "")
 
 # Goodness of Fit #########################################
 
+# allOut_mode <- c6_models %>%
+#   keep(str_detect(names(.), pattern = "m[:digit:]"))
 
 GoF <- c6_models %>%
-  map(~.x$summaries %>% select(LL, AIC, BIC, aBIC, Entropy))
+  keep(str_detect(names(.), pattern = "m[:digit:]")) %>%
+  map(~.x$summaries %>% select(AIC, BIC, aBIC, Entropy, LL, Parameters))
 
+allsumm <- c6_models %>%
+  keep(str_detect(names(.), pattern = "m[:digit:]")) %>%
+  map(~.x$summaries)
 
 # Class Sizes #############################################
 
